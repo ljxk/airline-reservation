0037246ebb3f8b7283f3bff004c630112046601d
ljxk <102322415+ljxk@users.noreply.github.com>
2022-06-02 14:38:52 +0000

    new system more polish

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
-print ("is this working")
-fromlocation = input("where are you. belarus,chad,philadelphia,brazil,jamaica,democratic republic of the congo,australia")
-locations = ["belarus","chad","philadelphia","brazil","democratic republic of the congo","australia"]
-if not fromlocation in locations:
-  print ("you lyin")
-  quit()
-golocation = input("where are you going. belarus,chad,philadelphia,brazil,jamaica,democratic republic of the congo,australia")
-if not golocation in locations:
-  print ("we dont go there")
-  quit()
-if golocation == fromlocation:
-  print ("you already there")
-  quit()
-name = input("what is your name")
-city = input("what is your city")
-birthdate = input("when were you birthed m/d/y")
-if len(birthdate) > 9:
-  print ("nah")
-  quit()
-classairplane = input("what class your ordering 1st class, 2nd class, 3rd class")
-classes = ("1st class", "2nd class","3rd class")
-
-if not classairplane in classes:
-  print ("nah")
-  quit()
-print ("--------------------------------------------------")
-print (name)
-print ("going to "+golocation+" from "+fromlocation+".")
-print ("in "+city+".")
-print ("born "+birthdate+".")
-print (classairplane)
+def loop():
+  print ("--------------------------------------------------")
+  fromlocation = input("where are you. belarus,chad,philadelphia,brazil,jamaica,democratic republic of the congo,australia,new mexico          ")
+  locations = ["belarus","chad","philadelphia","brazil","democratic republic of the congo","australia"," belarus"," chad","new mexico"," philadelphia"," brazil"," democratic republic of the congo"," australia"," new mexico"]
+  if not fromlocation in locations:
+    print ("you lyin")
+    loop()
+  golocation = input("where are you going. belarus,chad,philadelphia,brazil,jamaica,democratic republic of the congo,australia,new mexico          ")
+  if not golocation in locations:
+    print ("we dont go there")
+    loop()
+  if golocation == fromlocation:
+    print ("you already there")
+    loop()
+  name = input("what is your name          ")
+  city = input("what is your city          ")
+  birthmonth = input("what month were you born?          ")
+  months = ('january','february','march', 'april', 'may','june','july','august','september','october','november','december')
+  if not birthmonth in months:
+    print ("nah")
+    loop()
+  birthday = input("what day were you born(number not monday,tuesday,wednesday,etc)          ")
+  days = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)
+  if not int(birthday) in days:
+    print ("nah")
+    loop()
+  birthyear = input("what year were you born?          ")
+  
+  
+  classairplane = input("what class your ordering 1st class, 2nd class, 3rd class          ")
+  classes = ("1st class", "2nd class","3rd class")
+  
+  if not classairplane in classes:
+    print ("nah")
+    loop()
+  print ("--------------------------------------------------")
+  print (name)
+  print ("going to "+golocation+" from "+fromlocation+".")
+  print ("in "+city+".")
+  print ("born "+birthmonth+" "+birthday+" "+birthyear+".")
+  print (classairplane)
+loop()
